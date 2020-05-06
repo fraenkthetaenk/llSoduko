486d0556a9ef9a94df7ed39664e8f5dbdd7605bb
Steiner Frank Thomas (IFAG IT ES PROC) <FrankThomas.Steiner@infineon.com>
2020-05-06 13:03:51 +0200

    =added colums, rows and squares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  Math= Math;
   title = 'picSoduko';
+  row1=[];
+  column1=[];
+  sqaure1=[];
+  row2=[];
+  column2=[];
+  sqaure2=[];
+  row3=[];
+  column3=[];
+  sqaure3=[];
+  row4=[];
+  column4=[];
+  sqaure4=[];
+  row5=[];
+  column5=[];
+  sqaure5=[];
+  row6=[];
+  column6=[];
+  sqaure6=[];
+  row7=[];
+  column7=[];
+  sqaure7=[];
+  row8=[];
+  column8=[];
+  sqaure8=[];
+  row9=[];
+  column9=[];
+  sqaure9=[];
   range =[1,2,3,4,5,6,7,8,9];
   tile1 = ["21","38","76"];//chika
   tile2 = ["29","58","85","91"];//dia
@@ -27,5 +56,6 @@ export class AppComponent {
   .concat(this.tile8)
   .concat(this.tile9)
 }
+
 
 
